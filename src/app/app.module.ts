06a66f8b4cc5cfe3ab8a0cdea4963d8d6ce2e221
Yazmin-Trujillo <lirio280688@hotmail.com>
2022-08-23 08:40:25 -0500

    PrimeNG, y navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ButtonModule} from 'primeng/button';
+import {SidebarModule} from 'primeng/sidebar';
+import {ToolbarModule} from 'primeng/toolbar';
+import {InputTextModule} from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule, //required to use ngModel in prime components (https://stackoverflow.com/a/67817027)
+    ButtonModule,
+    SidebarModule,
+    ToolbarModule,
+    InputTextModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
