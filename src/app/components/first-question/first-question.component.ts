c04805689ffc303bb31d8c86a52b6374bef9f293
Lourdes Santiago <lourdes.santiago.palacios@gmail.com>
2022-08-24 23:56:25 -0500

    Servicio para perfilador, puntuacion y contador

--- src/app/components/first-question/first-question.component.ts
+++ src/app/components/first-question/first-question.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProfilerService } from 'src/app/services/profiler.service';
 
 @Component({
   selector: 'app-first-question',
@@ -9,10 +11,20 @@ export class FirstQuestionComponent implements OnInit {
 
  
 
-  constructor() {}
+  constructor(
+    private profileService: ProfilerService,
+    private router: Router
+    ) {}
 
 
   ngOnInit(): void {
+  }
+
+  setResFirst(name: string, value: number){
+    const res = { name, value, id: 1 }
+    this.profileService.addRes(res);
+    this.router.navigateByUrl('/second-question');
+    console.log(this.profileService.questions)
   }
 
 }
