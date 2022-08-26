7d0b5f7b568f394ded30afb491ef4fa133cae6fc
Yazmin-Trujillo <lirio280688@hotmail.com>
2022-08-25 19:11:04 -0500

    Update static folder

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("restData.json");
-const middlewares = jsonServer.defaults({ static: "./build" });
+const middlewares = jsonServer.defaults({ static: "./dist" });
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
