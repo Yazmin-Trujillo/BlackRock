e4cdddb2a0ef22f5711eeb624d5c33139a46c26d
Yazmin-Trujillo <lirio280688@hotmail.com>
2022-08-25 19:18:16 -0500

    Update server defaults path

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("restData.json");
-const middlewares = jsonServer.defaults({ static: "./dist" });
+const middlewares = jsonServer.defaults({ static: "./dist/black-rock" });
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 
 server.listen(port);
+console.log('listening in port='+port)
